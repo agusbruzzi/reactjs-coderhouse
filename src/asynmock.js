89773cc8c8b64fcdffc5abfed18593af28ae436f
agusbruzzi <agusb.bruzzi@gmail.com>
2022-06-05 18:58:45 -0300

    se agregan imagenes para la tienda

--- src/asynmock.js
+++ src/asynmock.js
@@ -13,10 +13,52 @@ const products = [
     {id: '12', name: 'Riñonera', price: '750', category: 'accesorios', img: '/assets/products/RINONERAS.jpg', stock: '10', description: 'Riñonera confeccionada en tela Cordura, con cierre delantero y correa ajustable.'},
 ]
 
+const outfits = [
+    {id: '1', name: 'Top Ribb + Bermuda Ultra Wide', price: '1.950', category: 'outfits', img: '/assets/outfits/OUTFIT-39.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'},
+    {id: '2', name: 'Remeron + Bermuda Ultra Wide', price: '2.000', category: 'outfits', img: '/assets/outfits/OUTFIT-31.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'},
+    {id: '3', name: 'Remeron + Jogger Regular Frisado', price: '3.150', category: 'outfits', img: '/assets/outfits/OUTFIT-51.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'},
+    {id: '4', name: 'Hoodie Oversize Frisado + Short de Algodón', price: '3.350', category: 'outfits', img: '/assets/outfits/OUTFIT-21.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'},
+    {id: '5', name: 'Top Ribb + Jean Mom', price: '3.550', category: 'outfits', img: '/assets/outfits/OUTFIT-41.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'},
+    {id: '6', name: 'Remeron + Jean Relaxed Hombre', price: '3.600', category: 'outfits', img: '/assets/outfits/OUTFIT-57.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'},
+    {id: '7', name: 'Hoodie Oversize + Remera Oversize + Short de Algodon', price: '3.800', category: 'outfits', img: '/assets/outfits/OUTFIT-35.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'},
+    {id: '8', name: 'Jean Skinny Mujer + Remera Regular Fit Crop x 2', price: '4.400', category: 'outfits', img: '/assets/outfits/OUTFIT-38.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'},
+    {id: '9', name: 'Hoodie Oversize Frisado + Jogger Regular Frisado', price: '4.500', category: 'outfits', img: '/assets/outfits/OUTFIT-13.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'},
+    {id: '10', name: 'Remera Manga Larga Oversize + Buzo Sleeveless Frisado + Jean Skinny Mujer', price: '4.550', category: 'outfits', img: '/assets/outfits/OUTFIT-48.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'},
+    {id: '11', name: 'Chaqueta Oversize + Hoodie Oversize Frisado', price: '4.950', category: 'outfits', img: '/assets/outfits/OUTFIT-54.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'},
+    {id: '12', name: 'Hoodie Oversize Frisado + Jean Mom', price: '', category: 'outfits', img: '/assets/outfits/.jpg', stock: '10', description: 'La tonalidad del mismo color puede variar de prenda a prenda.'}
+]
+
+const maxicombos = [
+    {id: '1', name: 'Maxicombo #5', price: '4.650', category: 'maxicombos', img: '/assets/maxicombos/MAXICOMBOS-5.jpg', stock: '5', description:'Los productos que contengan en su nombre la palabra “OUTLET” pueden contener algún tipo de falla de fabrica, encojen con el lavado y no tienen cambio ni devolución.'},
+    {id: '2', name: 'Maxicombo #14', price: '5.050', category: 'maxicombos', img: '/assets/maxicombos/MAXICOMBOS-14.jpg', stock: '5', description:'Los productos que contengan en su nombre la palabra “OUTLET” pueden contener algún tipo de falla de fabrica, encojen con el lavado y no tienen cambio ni devolución.'},
+    {id: '3', name: 'Maxicombo #13', price: '5.200', category: 'maxicombos', img: '/assets/maxicombos/MAXICOMBOS-13.jpg', stock: '5', description:'Los productos que contengan en su nombre la palabra “OUTLET” pueden contener algún tipo de falla de fabrica, encojen con el lavado y no tienen cambio ni devolución.'},
+    {id: '4', name: 'Maxicombo #1', price: '5.600', category: 'maxicombos', img: '/assets/maxicombos/MAXICOMBOS-1.jpg', stock: '5', description:'Los productos que contengan en su nombre la palabra “OUTLET” pueden contener algún tipo de falla de fabrica, encojen con el lavado y no tienen cambio ni devolución.'},
+    {id: '5', name: 'Maxicombo #12', price: '5.850', category: 'maxicombos', img: '/assets/maxicombos/MAXICOMBOS-12.jpg', stock: '5', description:'Los productos que contengan en su nombre la palabra “OUTLET” pueden contener algún tipo de falla de fabrica, encojen con el lavado y no tienen cambio ni devolución.'},
+    {id: '6', name: 'Maxicombo #10', price: '6.250', category: 'maxicombos', img: '/assets/maxicombos/MAXICOMBO-10.jpg', stock: '5', description:'Los productos que contengan en su nombre la palabra “OUTLET” pueden contener algún tipo de falla de fabrica, encojen con el lavado y no tienen cambio ni devolución.'},
+    {id: '7', name: 'Maxicombo #17', price: '6.350', category: 'maxicombos', img: '/assets/maxicombos/MAXICOMBO-17.jpg', stock: '5', description:'Los productos que contengan en su nombre la palabra “OUTLET” pueden contener algún tipo de falla de fabrica, encojen con el lavado y no tienen cambio ni devolución.'},
+    {id: '8', name: 'Maxicombo #16', price: '6.350', category: 'maxicombos', img: '/assets/maxicombos/MAXICOMBO-16.jpg', stock: '5', description:'Los productos que contengan en su nombre la palabra “OUTLET” pueden contener algún tipo de falla de fabrica, encojen con el lavado y no tienen cambio ni devolución.'},
+]
+
 export const getProducts = () => {
     return new Promise( (resolve, reject) => {
         setTimeout(() => {
              resolve(products);
+        }, 2000)
+    })
+}
+
+export const getOutfits = () => {
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+             resolve(outfits);
+        }, 2000)
+    })
+}
+
+export const getMaxicombos = () => {
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+             resolve(maxicombos);
         }, 2000)
     })
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,11 +7,11 @@ const NavBar = () => {
             <ul>
             <div className="button-row">
             <div><a href="http://localhost:3000/" title="Inicio"> </a></div>
-            <div><a href="http://localhost:3000/" title="Nuevos Ingresos"> </a></div>
             <div><a href="http://localhost:3000/" title="Catálogo"> </a></div>
             <div><a href="http://localhost:3000/" title="Outfits"> </a></div>
             <div><a href="http://localhost:3000/" title="Packs"> </a></div>
-            <div><a href="http://localhost:3000/" title="Outlet"> </a></div>
+            <div><a href="http://localhost:3000/" title="Maxicombos"> </a></div>
+            <div><a href="http://localhost:3000/" title="Contacto"> </a></div>
             </div>
             </ul>
         </nav>
