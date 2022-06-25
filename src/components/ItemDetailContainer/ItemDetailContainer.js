e5e303d7f87e51ca54feaa00ad01e2c883fbaacb
agusbruzzi <agusb.bruzzi@gmail.com>
2022-06-25 19:10:15 -0300

    se agrega spinner

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -5,12 +5,22 @@ import { useParams } from "react-router-dom"
 
 const ItemDetailContainer = () => {
     const [product, setProduct] = useState()
+    const [loading, setloading] = useState(true)
+
     const params = useParams()
     useEffect(() => {
+        setloading(true)
         getProductsById(params.productId).then(response => {
             setProduct(response)
+        }).finally(()=>{
+            setloading(false)
         })
     },[params.productId])
+
+    if(loading){
+        //<div className="spinner"></div>
+        return <div className='loading'><div className="spinner"></div></div>
+    }
 
     return (
         <>
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -5,21 +5,30 @@ import { useParams } from "react-router-dom"
 
 const ItemListContainer = () => {
     const [products, setProducts] = useState([])
-
+    const [loading, setloading] = useState(true)
     const {category} = useParams()
     
     useEffect(() =>{
+        setloading(true)
         if(!category){
             getProducts().then(response => {
                 setProducts(response)
+            }).finally(() => {
+                setloading(false)
             })
         } else{
             getProductsByCategory(category).then(response => {
                 setProducts(response)
+            }).finally(()=>{
+                setloading(false)
             })
         }
-
     }, [category])
+
+    if(loading){
+        //<div className="spinner"></div>
+        return <div className='loading'><div className="spinner"></div></div>
+    }
 
     return (
         <div className='itemListContainer'>
